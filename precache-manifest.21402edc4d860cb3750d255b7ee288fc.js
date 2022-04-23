3a83ef0f004ea0ed75d371b18f7288cf50524df9
Raghu <r4ghunandhan@gmail.com>
2022-04-23 19:56:37 +0530

    Updates

---
+++ precache-manifest.21402edc4d860cb3750d255b7ee288fc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6dd4f9c2dfd65d4dc7df3db8bd988df",
+    "url": "/covid19tracker/index.html"
+  },
+  {
+    "revision": "8b15bba16182535b11f7",
+    "url": "/covid19tracker/static/css/2.326cb26e.chunk.css"
+  },
+  {
+    "revision": "070944c3602982272b0f",
+    "url": "/covid19tracker/static/css/main.47e3359f.chunk.css"
+  },
+  {
+    "revision": "8b15bba16182535b11f7",
+    "url": "/covid19tracker/static/js/2.8ca6e928.chunk.js"
+  },
+  {
+    "revision": "a01d0c736a39a5811f5efe0ff20b3a49",
+    "url": "/covid19tracker/static/js/2.8ca6e928.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "070944c3602982272b0f",
+    "url": "/covid19tracker/static/js/main.b26dca5f.chunk.js"
+  },
+  {
+    "revision": "1d22c585232c3576941a",
+    "url": "/covid19tracker/static/js/runtime-main.295b43a3.js"
+  }
+]);
